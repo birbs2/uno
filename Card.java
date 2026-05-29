2b522d781ffcf9fa182d1bc12021acacd6ed0ce2
birbs2 <samuel.h.flower@gmail.com>
2026-05-29 16:13:09 -0400

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,11 @@
+public abstract class Card {
+
+    private final int number;
+    private final String color;
+
+    public Card(int number, String color) {
+        this.number = number;
+        this.color = color;
+    }
+    
+}
